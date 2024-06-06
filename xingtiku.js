42a987767e4dbf9f6d9b2e1ab126b4b786f4e4fb
svvff <58783469+svvff@users.noreply.github.com>
2024-06-06 21:12:19 +0800

    Update xingtiku.js

--- xingtiku.js
+++ xingtiku.js
@@ -19,35 +19,15 @@ hostname = mobile.xinghengclass.com
 
 var objc = JSON.parse($response.body);
 
-objc.data = {    
-    "img": "default",
-    "code": 1,
-    "gender": 1,
-    "everstarName": "星恒",
-    "agentTel": "4008079767",
-    "agentQq": "",
-    "videoSource": 0,
-    "integral": 0.0,
-    "id": 11000630560,
-    "serverPhone": "4008079767",
+objc.data = {
+    ...objc.data,
     "vip": true,
-    "key": "",
-    "info": "让我们一起加油做题吧~",
-    "qq": "",
-    "dedicated": 0,
-    "agentAddress": "",
-    "agentName": "",
     "vipType": "3",
     "bindPhoneNumber": "10086",
-    "cs": "694947160",
-    "phone": "4008079767",
     "name": "永久会员",
     "endTime": 999999999,
     "vipStatus": "1",
-    "crmName": "",
     "username": "永久会员"
 };
 
-$done({
-    body : JSON.stringify(objc)
-});
+$done({body : JSON.stringify(chxm1023)});
