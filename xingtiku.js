91fdc9153694e0f2d75af071a3fb2db5e016d9e5
svvff <58783469+svvff@users.noreply.github.com>
2024-06-07 12:35:23 +0800

    Update xingtiku.js

--- xingtiku.js
+++ xingtiku.js
@@ -9,8 +9,7 @@
 
 [rewrite_local]
 
-^https?://mobile.xinghengclass.com/api/v5.3.0/getUserByToken.do url script-response-body
-https://raw.githubusercontent.com/svvff/Rewrite/main/xingtiku.js
+https?://mobile.xinghengclass.com/api/v5.3.0/getUserByToken.do url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/xingtiku.js
 
 [mitm]
 hostname = mobile.xinghengclass.com
