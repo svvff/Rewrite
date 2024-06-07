e47c951afa2fb4138406931ee1b0859c0d236dac
svvff <58783469+svvff@users.noreply.github.com>
2024-06-07 12:47:17 +0800

    Update AliYun.js

--- AliYun.js
+++ AliYun.js
@@ -9,7 +9,7 @@
 
 [rewrite_local]
 
-^https:\/\/api\.alipan\.com\/business\/v1\.0\/users\/feature\/list$,script-path=https://raw.githubusercontent.com/svvff/Rewrite/main/aliyun.js
+^https:\/\/api\.alipan\.com\/business\/v1\.0\/users\/feature\/list$ url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/AliYun.js
 
 [mitm]
 hostname = api.alipan.com
