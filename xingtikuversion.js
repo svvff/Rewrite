6f82c64e27c609be0056529bbc1ad0b9f0458f51
svvff <58783469+svvff@users.noreply.github.com>
2024-06-07 10:00:13 +0800

    Create xingtikuversion.js

---
+++ xingtikuversion.js
@@ -0,0 +1,28 @@
+/*************************************
+
+项目名称：星题库-屏蔽更新弹窗
+更新日期：2024-6-7
+使用声明：仅供参考，禁止转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?://mobile.xinghengclass.com/mobileUser/v2/versionList.do
+https://raw.githubusercontent.com/svvff/Rewrite/main/xingtikuversion.js
+
+[mitm]
+hostname = mobile.xinghengclass.com
+
+*************************************/
+
+var objc = JSON.parse($response.body);
+
+objc = {
+  ...objc,
+  "apkName": "hello world!",
+  "apkVersion": "5.36.0",
+  "apkVersionCode": 53600,
+  "apkVersionDesc": "版本拦截测试！",
+};
+
+$done({body : JSON.stringify(objc)});
