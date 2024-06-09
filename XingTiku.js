41c1bf5736115a0476817d6978664577f9bafae3
svvff <58783469+svvff@users.noreply.github.com>
2024-06-10 01:11:22 +0800

    Update XingTiku.js

--- XingTiku.js
+++ XingTiku.js
@@ -12,7 +12,7 @@
 #!name=星题库
 #!desc=解锁会员
 
-^https?://mobile.xinghengclass.com/api/v5.3.0/getUserByToken.do url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/XingTiku.js
+^https?:\/\/mobile\.xinghengclass\.com\/(api\/v5\.3\.0\/getUserByToken\.do|mobileUser\/v2\/versionList\.do|notification\/list\.do) url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/XingTiku.js
 
 [mitm]
 hostname = mobile.xinghengclass.com
@@ -21,6 +21,45 @@ hostname = mobile.xinghengclass.com
 
 var objc = JSON.parse($response.body);
 
+const ad = /notification\/list\.do/;
+const version = /mobileUser\/v2\/versionList\.do/;
+const vip = /api\/v5\.3\.0\/getUserByToken\.do/;
+
+if(ad.test($request.url)){
+  objc.lists = {
+  ...objc.lists,
+  "imgUrl": "",
+  "url": "",
+  "content": "",
+  "productType": ""
+};
+}
+
+if(version.test($request.url)){
+  objc[0] = {
+    "apkName": "hello world!",
+    "apkVersion": "1.00.0",
+    "apkVersionCode": 10000,
+    "apkVersionDesc": "版本拦截测试！",
+    "apkVersionType": 1,
+    "id": 1,
+    "productType": "xingtiku_ios"
+};
+}
+
+if(version.test($request.url)){
+  objc.data = {
+    ...objc.data,
+    "vip": true,
+    "vipType": "3",
+    "bindPhoneNumber": "10086",
+    "name": "永久会员",
+    "endTime": 999999999,
+    "vipStatus": "1",
+    "username": "永久会员"
+};
+}
+/*
 objc.data = {
     ...objc.data,
     "vip": true,
@@ -31,5 +70,5 @@ objc.data = {
     "vipStatus": "1",
     "username": "永久会员"
 };
-
+*/
 $done({body : JSON.stringify(objc)});
