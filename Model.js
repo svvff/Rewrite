8cda4166ccfebed093bf727b1113ead349e8de5a
svvff <58783469+svvff@users.noreply.github.com>
2024-06-09 16:33:26 +0800

    Update Model.js

--- Model.js
+++ Model.js
@@ -21,9 +21,9 @@ hostname = *.lanjiyin.com.cn
 
 var body = $response.body;
 
-body = body.replace(/is_unlock":"0"/g,'is_unlock":"1"')
-.replace(/is_free":"\d"/g,'is_free":"1"')
-.replace(/is_show":"\d"/g,'is_show":"1"')
-.replace(/is_see":"\d+"/g,'is_see":"1"');
+body = body.replace(/"is_unlock":\d/g,'"is_unlock":"1"')
+.replace(/"is_free":\d/g,'"is_free":"1"')
+.replace(/"is_show":\d/g,'"is_show":"1"')
+.replace(/"is_see":\d+/g,'"is_see":"1"');
 
 $done({ body });
