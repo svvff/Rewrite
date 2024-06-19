518a0096a625a1d0787b2f52b056a97ef3b7135b
svvff <58783469+svvff@users.noreply.github.com>
2024-06-19 22:00:17 +0800

    Create BiliBiliAds.js

---
+++ BiliBiliAds.js
@@ -0,0 +1,27 @@
+/*************************************
+
+项目名称：哔哩哔哩-去除开屏广告
+更新日期：2024-6-19
+使用声明：仅供参考，禁止转载与售卖！
+
+**************************************
+
+[rewrite_local]
+
+#!name=哔哩哔哩
+#!desc=屏蔽首次启动广告弹窗
+
+^https?://​​​a​​​p​​​p​​​.​​​b​​​i​​​l​​​i​​​b​​​i​​​l​​​i​​​.​​​c​​​o​​​m​​​/​​​x​​​/​​​v​​​2​​​​/(​​​f​​​e​​​e​​​d​​​\/​​​i​​​n​​​d​​​e​​​x|s​​​p​​​l​​​a​​​s​​​h\​​​/​​​s​​​h​​​o​​​w) url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/BiliBiliAds.js
+
+[mitm]
+hostname = a​​​p​​​p​​​.​​​b​​​i​​​l​​​i​​​b​​​i​​​l​​​i​​​.​​​c​​​o​​​m​​​
+
+*************************************/
+
+var objc = JSON.parse($response.body);
+
+objc = {
+
+};
+
+$done({body : JSON.stringify(objc)});
