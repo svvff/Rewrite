4d37d5e9a5ecd75c31c5de94832a2fd42467e351
svvff <58783469+svvff@users.noreply.github.com>
2026-05-20 01:12:37 +0800

    Update anki_unlock_all.js

--- anki_unlock_all.js
+++ anki_unlock_all.js
@@ -1,16 +1,16 @@
 /*******************************
 
 脚本功能：Anymo——解锁VIP会员 + 解除非会员限制
+软件版本：1.0.0
 更新时间：2026-05-20
-版本：1.0.0
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！
 
 *******************************
 
-[rewrite_local]
-
 #!name=Anymo解锁
 #!desc=解锁永久VIP + 非会员数量限制
+
+[rewrite_local]
 
 ^https?:\/\/file\.ankichinas\.cn\/server\/api\/user\/vipInfo$ url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/anki_unlock_all.js
 ^https?:\/\/file\.ankichinas\.cn\/server\/api\/user\/getInfo$ url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/anki_unlock_all.js
@@ -20,38 +20,38 @@ hostname = file.ankichinas.cn
 
 *******************************/
 
-var objc = JSON.parse($response.body);
+var body = JSON.parse($response.body);
 
-const vipInfo = /file\.ankichinas\.cn\/server\/api\/user\/vipInfo/;
-const getInfo = /file\.ankichinas\.cn\/server\/api\/user\/getInfo/;
+const vipInfo = /\/user\/vipInfo$/;
+const getInfo = /\/user\/getInfo$/;
 
 if (vipInfo.test($request.url)) {
-    if (objc.data) {
-        objc.data.isVip = true;
-        objc.data.isForever = true;
-        objc.data.vipDay = 99999;
-        objc.data.expireDay = "2099-12-31";
+    if (body.data) {
+        body.data.isVip = true;
+        body.data.isForever = true;
+        body.data.vipDay = 99999;
+        body.data.expireDay = "2099-12-31";
     }
 }
 
 if (getInfo.test($request.url)) {
-    if (objc.data) {
-        if (objc.data.vip) {
-            objc.data.vip.isForever = true;
-            objc.data.vip.openVip = true;
-            objc.data.vip.vipAvailable = true;
-            objc.data.vip.vipEndAt = "2099-12-31";
+    if (body.data) {
+        if (body.data.vip) {
+            body.data.vip.isForever = true;
+            body.data.vip.openVip = true;
+            body.data.vip.vipAvailable = true;
+            body.data.vip.vipEndAt = "2099-12-31";
         }
-        if (objc.data.nonmember) {
-            objc.data.nonmember.decksCount = 999999;
-            objc.data.nonmember.notesCount = 999999;
-            objc.data.nonmember.customStudy = 999999;
-            objc.data.nonmember.exportCard = 999999;
-            objc.data.nonmember.appDataSync = true;
-            objc.data.nonmember.batchCard = true;
-            objc.data.nonmember.coverPic = true;
+        if (body.data.nonmember) {
+            body.data.nonmember.decksCount = 999999;
+            body.data.nonmember.notesCount = 999999;
+            body.data.nonmember.customStudy = 999999;
+            body.data.nonmember.exportCard = 999999;
+            body.data.nonmember.appDataSync = true;
+            body.data.nonmember.batchCard = true;
+            body.data.nonmember.coverPic = true;
         }
     }
 }
 
-$done({body : JSON.stringify(objc)});
+$done({body : JSON.stringify(body)});
