e1e93113642b9ca3efe2e0907132c080cbc3b2d3
svvff <58783469+svvff@users.noreply.github.com>
2026-05-20 00:53:48 +0800

    Update anki_unlock_all.js

--- anki_unlock_all.js
+++ anki_unlock_all.js
@@ -5,11 +5,11 @@
 #!name=Anymo解锁
 #!desc=解锁永久VIP + 非会员数量限制
 
-*******************************
+*******************************/
 
 [rewrite_local]
-^https?:\/\/file\.ankichinas\.cn\/server\/api\/user\/vipInfo$ url script-response-body anki_unlock_all.js
-^https?:\/\/file\.ankichinas\.cn\/server\/api\/user\/getInfo$ url script-response-body anki_unlock_all.js
+^https?:\/\/file\.ankichinas\.cn\/server\/api\/user\/vipInfo$ url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/anki_unlock_all.js
+^https?:\/\/file\.ankichinas\.cn\/server\/api\/user\/getInfo$ url script-response-body https://raw.githubusercontent.com/svvff/Rewrite/main/anki_unlock_all.js
 
 [mitm]
 hostname = file.ankichinas.cn
@@ -19,12 +19,10 @@ hostname = file.ankichinas.cn
 var body = $response.body;
 var url = $request.url;
 
-// 处理 /user/vipInfo 接口
-if (url.includes('/user/vipInfo')) {
+if (url.indexOf('/user/vipInfo') !== -1) {
     try {
         var obj = JSON.parse(body);
         if (obj.data) {
-            // 强制开启VIP
             obj.data.isVip = true;
             obj.data.isForever = true;
             obj.data.vipDay = 99999;
@@ -34,19 +32,16 @@ if (url.includes('/user/vipInfo')) {
     } catch(e) {}
 }
 
-// 处理 /user/getInfo 接口
-if (url.includes('/user/getInfo')) {
+if (url.indexOf('/user/getInfo') !== -1) {
     try {
         var obj = JSON.parse(body);
         if (obj.data) {
-            // VIP 信息解锁
             if (obj.data.vip) {
                 obj.data.vip.isForever = true;
                 obj.data.vip.openVip = true;
                 obj.data.vip.vipAvailable = true;
                 obj.data.vip.vipEndAt = "2099-12-31";
             }
-            // 非会员限制解锁（将数量上限改成极大值）
             if (obj.data.nonmember) {
                 obj.data.nonmember.decksCount = 999999;
                 obj.data.nonmember.notesCount = 999999;
